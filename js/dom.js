ad32a5ff2ee0b26b4a3b177f47e36cc037eb3937
Talysson678 <dev.senac.talysson@gmail.com>
2026-05-07 11:42:21 -0300

    Adicionando obter tarefa, limpar o input adicionar tarefa na lista

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,19 @@
 //dom.js
+export function obterTextoTarefa() {
+  const input = document.querySelector("#input-tarefa");
+  return input.value;
+}
+
+export function limparInput() {
+    document.querySelector("#input-tarefa").value = "";
+}
+
+export function criarElementoTarefaNaLista (texto) {
+    const list = document.querySelector("#lista-tarefas");
+
+    const li = document.createElement("li");
+    li.textContent = texto;
+
+    lista.appendChild(li);
+}
+
